d580350ba488c099752c210acfc6e4465fd1127c
PAVAN SHIRSATH <158544111+flywitpavan@users.noreply.github.com>
2024-10-19 17:46:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+() => { 
+    cy.visit(baseUrl + "/main.html"); 
+    
+    // Use '#summary' to target the element with the id 'summary'
+    cy.get('#summary').then(ans => { 
+        expect(ans.text().trim().length).to.gt(0); 
+    }); 
+}
